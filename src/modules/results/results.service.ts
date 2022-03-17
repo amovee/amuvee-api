3c168cc13463004280c5f29a4244534851a78efd
AmmonStretz <ammon-mino@web.de>
2022-03-17 10:29:31 +0100

    add/date filter

--- src/modules/results/results.service.ts
+++ src/modules/results/results.service.ts
@@ -4,7 +4,19 @@ import { Result, ResultDocument } from 'src/shared/schemas/result.schema';
 import { Model } from 'mongoose';
 import { Region, RegionDocument } from 'src/shared/schemas/region.schema';
 import { Answers } from 'src/shared/interfaces/answers.interface';
-import { generateChildrenAge, generateChildrenCountFilter, generateIncomeMaxFilter, generateIncomeMinFilter, generateInsuranceFilter, generateJobSituationFilter, generateRegionFilter, generateRelationships, generateRentFilter, generateRequiredKeywordsFilter, generateUnrequiredKeywordsFilter } from './results.filters';
+import {
+  generateChildrenAge,
+  generateChildrenCountFilter,
+  generateIncomeMaxFilter,
+  generateIncomeMinFilter,
+  generateInsuranceFilter,
+  generateJobSituationFilter,
+  generateRegionFilter,
+  generateRelationships,
+  generateRentFilter,
+  generateRequiredKeywordsFilter,
+  generateUnrequiredKeywordsFilter,
+} from './results.filters';
 
 @Injectable()
 export class ResultsService {
@@ -14,17 +26,42 @@ export class ResultsService {
   ) {}
 
   async getAllFilters(categoryId: string, answers: Answers): Promise<any> {
-    let regions = await this.regionModel.find({ postalCodes: { $in: [answers.zip] } }).exec();
+    let regions = await this.regionModel
+      .find({ postalCodes: { $in: [answers.zip] } })
+      .exec();
 
     let filters = [];
     /**
+     * FILTER END DATE
+     */
+    filters.push({
+      $or: [{ endDate: null }, { endDate: { $gte: new Date() } }],
+    });
+    filters.push({
+      $or: [
+        { startDate: null },
+        {
+          startDate: {
+            $lte: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7 * 4),
+          },
+        },
+      ],
+    });
+
+    /**
      * FILTER REGION
      */
-    if (answers.zip) filters.push(generateRegionFilter(answers.zip, regions.map((region) => region._id)));
+    if (answers.zip)
+      filters.push(
+        generateRegionFilter(
+          answers.zip,
+          regions.map((region) => region._id),
+        ),
+      );
     /**
      * FILTER CATEGORY
      */
-    if (categoryId != null) filters.push({categoryId: categoryId});
+    if (categoryId != null) filters.push({ categoryId: categoryId });
     /**
      * FILTER: KEYWORDS
      */
@@ -46,7 +83,8 @@ export class ResultsService {
     /**
      * FILTER CHILDREM_COUNT
      */
-    if (answers.childrenCount != null) filters.push(generateChildrenCountFilter(answers.childrenCount));
+    if (answers.childrenCount != null)
+      filters.push(generateChildrenCountFilter(answers.childrenCount));
     /**
      * FILTER CHILDREM_AGE
      */
@@ -67,22 +105,38 @@ export class ResultsService {
     /**
      * FILTER RELATIONSHIP
      */
-    if (answers.relationship != null) { // TODO: test
+    if (answers.relationship != null) {
+      // TODO: test
       filters.push(generateRelationships(answers.relationship));
     }
     /**
      * FILTER JOB_SITUAZTION
      */
-    if (answers.jobSituation != null) { // TODO: test
+    if (answers.jobSituation != null) {
+      // TODO: test
       filters.push(generateJobSituationFilter(answers.jobSituation));
     }
     return { $and: filters };
   }
-  async getFilteredResult(categoryId: string, answers: Answers, limit: number, offset: number, projection: any = {}): Promise<any> {
+  async getFilteredResult(
+    categoryId: string,
+    answers: Answers,
+    limit: number,
+    offset: number,
+    projection: any = {},
+  ): Promise<any> {
     return await this.resultModel
-      .find(await this.getAllFilters(categoryId, answers), projection).skip(offset).limit(limit).exec();
+      .find(await this.getAllFilters(categoryId, answers), projection)
+      .skip(offset)
+      .limit(limit)
+      .exec();
   }
-  async getFilteredResultCount(categoryId: string, answers: Answers): Promise<number> {
-    return await this.resultModel.count(await this.getAllFilters(categoryId, answers)).exec();
+  async getFilteredResultCount(
+    categoryId: string,
+    answers: Answers,
+  ): Promise<number> {
+    return await this.resultModel
+      .count(await this.getAllFilters(categoryId, answers))
+      .exec();
   }
 }
