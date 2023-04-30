f3c352d31ea733dd9e931cafa1dbc73e97bdf9cb
AmmonStretz <ammon-mino@web.de>
2023-04-30 15:39:39 +0200

    add event schema

---
+++ src/shared/schemas/event.schema.ts
@@ -0,0 +1,46 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+import mongoose, { Document } from 'mongoose';
+import { Roles } from 'src/shared/schemas/meta.schema';
+import { StateType, UserDTO } from 'src/types/types.dto';
+export type EventDocument = Event & Document;
+
+@Schema()
+export class Event {
+  @Prop()
+  _id: mongoose.Schema.Types.ObjectId;
+  @Prop()
+  id: number;
+  @Prop()
+  status: StateType;
+  @Prop()
+  link: string;
+  @Prop()
+  style: string;
+  @Prop()
+  type: string;
+  @Prop()
+  image: string;
+  @Prop()
+  start: Date;
+  @Prop()
+  end: Date;
+  @Prop({ _id: false, type: Roles })
+  roles: Roles;
+  @Prop({
+    type: mongoose.Schema.Types.Map,
+    of: {
+      _id: false,
+      name: String,
+      shortDescription: String,
+    },
+  })
+  content: {
+    [key: string]: {
+      name: string;
+      shortDescription: string;
+    };
+  };
+}
+
+export const EventSchema = SchemaFactory.createForClass(Event);
