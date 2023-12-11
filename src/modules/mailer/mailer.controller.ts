04dc7f1a0c7995e8cb6a224e112af63354aa5257
Ammon-Mino Stretz <ammon-mino@web.de>
2023-12-11 08:43:01 +0100

    tmp-fix/ comment out mailer

--- src/modules/mailer/mailer.controller.ts
+++ src/modules/mailer/mailer.controller.ts
@@ -1,32 +1,32 @@
 import {Body, Controller, Post, Logger, HttpException, HttpStatus} from '@nestjs/common';
-import {MailerService} from "@nestjs-modules/mailer";
-import {ContactFormDto} from "../../shared/dtos/mailer.dto";
-import { validate as emailValidate } from 'email-validator';
+// import {MailerService} from "@nestjs-modules/mailer";
+// import {ContactFormDto} from "../../shared/dtos/mailer.dto";
+// import { validate as emailValidate } from 'email-validator';
 
 
 
 @Controller('mailer')
 export class MailerController {
   private readonly logger = new Logger(MailerController.name);
-  constructor(private readonly mailerService: MailerService) {}
+  // constructor(private readonly mailerService: MailerService) {}
 
-  @Post()
-  async sendEmail(@Body() contactFormDto: ContactFormDto) {
-    if (!contactFormDto || !contactFormDto.email || !emailValidate(contactFormDto.email)) {
-      throw new HttpException('Invalid request data', HttpStatus.BAD_REQUEST);
-    }
-    this.logger.log(`Received contact form submission from ${contactFormDto.email}`);
-    try {
-    await this.mailerService.sendMail({
-      to: process.env.MAILER_RECIVER, // list of receivers
-      subject: `Neues Kontaktformular von ${contactFormDto.name} ${contactFormDto.surname}`, // Subject line
-      text: `Nachrichten: ${contactFormDto.message}\nVorname: ${contactFormDto.name}\nNachname: ${contactFormDto.surname}\nPlz: ${contactFormDto.plz}\n
-             Email: ${contactFormDto.email}\nHandy Nummer: ${contactFormDto.phone}\n`,
-    });
-    return { message: 'Email sent successfully!' };
-    } catch (error) {
-      this.logger.error(`Failed to send email from ${contactFormDto.email}`, error.stack);
-      throw new HttpException('Failed to send email', HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-  }
+  // @Post()
+  // async sendEmail(@Body() contactFormDto: ContactFormDto) {
+  //   if (!contactFormDto || !contactFormDto.email || !emailValidate(contactFormDto.email)) {
+  //     throw new HttpException('Invalid request data', HttpStatus.BAD_REQUEST);
+  //   }
+  //   this.logger.log(`Received contact form submission from ${contactFormDto.email}`);
+  //   try {
+  //   await this.mailerService.sendMail({
+  //     to: process.env.MAILER_RECIVER, // list of receivers
+  //     subject: `Neues Kontaktformular von ${contactFormDto.name} ${contactFormDto.surname}`, // Subject line
+  //     text: `Nachrichten: ${contactFormDto.message}\nVorname: ${contactFormDto.name}\nNachname: ${contactFormDto.surname}\nPlz: ${contactFormDto.plz}\n
+  //            Email: ${contactFormDto.email}\nHandy Nummer: ${contactFormDto.phone}\n`,
+  //   });
+  //   return { message: 'Email sent successfully!' };
+  //   } catch (error) {
+  //     this.logger.error(`Failed to send email from ${contactFormDto.email}`, error.stack);
+  //     throw new HttpException('Failed to send email', HttpStatus.INTERNAL_SERVER_ERROR);
+  //   }
+  // }
 }
--- src/modules/mailer/mailer.module.ts
+++ src/modules/mailer/mailer.module.ts
@@ -1,24 +1,24 @@
 import { Module } from '@nestjs/common';
-import { MailerModule as BaseMailerModule } from '@nestjs-modules/mailer';
+// import { MailerModule as BaseMailerModule } from '@nestjs-modules/mailer';
 import { MailerService } from './mailer.service';
 
 @Module({
   imports: [
-    BaseMailerModule.forRoot({
-      // SMTP transport configuration
-      transport: {
-        host: process.env.MAILER_HOST,
-        port: 587,
-        secure: false, // true for 465, false for other ports
-        auth: {
-          user: process.env.MAILER_USER,
-          pass: process.env.MAILER_PASS,
-        },
-      },
-      defaults: {
-        from: '"No Reply" <noreply@amuvee.de>',
-      },
-    }),
+    // BaseMailerModule.forRoot({
+    //   // SMTP transport configuration
+    //   transport: {
+    //     host: process.env.MAILER_HOST,
+    //     port: 587,
+    //     secure: false, // true for 465, false for other ports
+    //     auth: {
+    //       user: process.env.MAILER_USER,
+    //       pass: process.env.MAILER_PASS,
+    //     },
+    //   },
+    //   defaults: {
+    //     from: '"No Reply" <noreply@amuvee.de>',
+    //   },
+    // }),
   ],
   providers: [MailerService],
   exports: [MailerService], // Export the MailerService for use in other modules
