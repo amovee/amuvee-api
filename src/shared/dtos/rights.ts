dd22069d5f8263cc1f1a863a4631c881ff230280
Moaiadsu <moaiadalsultan@outlook.de>
2024-01-15 15:09:39 +0100

    add backend

--- src/shared/dtos/rights.ts
+++ src/shared/dtos/rights.ts
@@ -1,8 +1,3 @@
 export type rightNames = 'READ' | 'CREATE' | 'UPDATE' | 'DELETE' | 'PUBLISH';
-<<<<<<< HEAD
 export type documentNames = 'EVENTS' | 'RESULTS' | 'LOCATIONS' | 'USERS' | 'INSURANCES'|'REGIONS';
 export type right = `${documentNames}_${rightNames}`;
-=======
-export type documentNames = 'EVENTS' | 'RESULTS' | 'LOCATIONS' | 'USERS' | 'INSURANCES';
-export type right = `${documentNames}_${rightNames}`;
->>>>>>> 647dde1 (add the requests to the backend and tested them)
